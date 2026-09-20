26f23d4625d3e0a78e640841d03a0592e074fce8
DinuriN <dinurin730@gmail.com>
2026-09-20 13:01:14 +0530

    Initialize branch

---
+++ backend/SmartGrid.API/DTOs/GridOperations/DashboardStatsResponseDto.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * File Name    : DashboardStatsResponseDto.cs
+ * Description  : Represents operational statistics displayed on the Grid Operator dashboard.
+ * Author       : Dinuri Nureka A L D
+ * IT Number    : IT23192478
+ * Date         : 2026-09-20
+ */
+
+namespace SmartGrid.API.DTOs.GridOperations
+{
+    public class DashboardStatsResponseDto
+    {
+        public long PendingReservations { get; set; }
+        public long ApprovedFutureReservations { get; set; }
+    }
+}
---
+++ backend/SmartGrid.API/DTOs/GridOperations/VerifyQrRequestDto.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * File Name    : VerifyQrRequestDto.cs
+ * Description  : Represents the request data required to verify a prosumer transaction QR code.
+ * Author       : Dinuri Nureka A L D
+ * IT Number    : IT23192478
+ * Date         : 2026-09-20
+ */
+
+namespace SmartGrid.API.DTOs.GridOperations
+{
+    public class VerifyQrRequestDto
+    {
+        public string QrCode { get; set; } = string.Empty;
+        public string OperatorId { get; set; } = string.Empty;
+    }
+}
